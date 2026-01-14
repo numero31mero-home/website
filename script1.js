6fde3c965985df4fbfe9998976a4263012f13416
numero31mero-home <numero31.mero@gmail.com>
2026-01-14 18:04:37 +0900

    Add svg5 animations and update svg1-4 settings
    Signed-off-by: numero31mero-home <numero31.mero@gmail.com>

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-["#svg1","#svg2","#svg3","#svg4"].forEach(id=>{
+["#svg1","#svg2","#svg3","#svg4","#svg5"].forEach(id=>{
   gsap.set(id, { left:"50%", top:"50%", xPercent:-50, yPercent:-50, x:0 });
 });
 
@@ -78,3 +78,24 @@ var svg4TL = gsap.timeline({scrollTrigger: {
 svg4TL.fromTo("#svg4 video, #svg4 img", {y: "30%"}, {y: 0}, 0)
 svg4TL.to("#svg__star4", {morphSVG: {shape: "#svg__rec4", origin: "50% 50%"}}, 0);
 svg4TL.fromTo("#svg4", {width: () => {if(window.innerWidth < 600) {return 70} else {return 200}}}, {width: () => {if(window.innerWidth < window.innerHeight){return "100vh"} else {return "100vw"}}, ease: "none"}, 0);
+
+
+// five star
+gsap.set("#svg5", {y: () => window.innerHeight})
+gsap.to("#svg5", {y: 0, x: 0, scrollTrigger: {
+  trigger: "body",
+  scrub: true,
+  start: () => window.innerHeight * 8 + " bottom",
+  end: () => window.innerHeight * 9 + " bottom"
+}, ease: "none"})
+
+var svg5TL = gsap.timeline({scrollTrigger: {
+  trigger: "body",
+  scrub: 2,
+  start: () => window.innerHeight * 9 + " bottom",
+  end: () => window.innerHeight * 10 + " bottom"
+}})
+
+svg5TL.fromTo("#svg5 video, #svg5 img", {y: "30%"}, {y: 0}, 0)
+svg5TL.to("#svg__star5", {morphSVG: {shape: "#svg__rec5", origin: "50% 50%"}}, 0);
+svg5TL.fromTo("#svg5", {width: () => {if(window.innerWidth < 600) {return 70} else {return 200}}}, {width: () => {if(window.innerWidth < window.innerHeight){return "100vh"} else {return "100vw"}}, ease: "none"}, 0);
